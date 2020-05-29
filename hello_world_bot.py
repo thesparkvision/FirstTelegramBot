b91b422575e4c50539732dbfa9a98e4fbeebc3da
thesparkvision <aman15.pandya@gmail.com>
2020-05-29 18:48:35 +0530

    Added non-command message handling feature to the bot

--- hello_world_bot.py
+++ hello_world_bot.py
@@ -1,8 +1,13 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
 
 def start(bot,update):
     update.message.reply_text("I'm a bot, Nice to meet you!")
+
+def convert_to_uppercase(bot,update):
+    update.message.reply_text(update.message.text.upper())
 
 def main():
     #Reading the token from API_Token.txt file
@@ -18,10 +23,15 @@ def main():
 
     #Linking updater to dispatcher which has appropriate event handlers method attached to it
     dispatcher=updater.dispatcher
-
+    print("Bot started") 
+     
     #Creating a CommandHandler Object to link command to function and adding it to dispatcher
     start_handler=CommandHandler('start',start)
     dispatcher.add_handler(start_handler)
+
+    #Creating a MessageHandler Object to link non-command messages to function
+    uppercase_handler=MessageHandler(Filters.text,convert_to_uppercase)
+    dispatcher.add_handler(uppercase_handler)
 
     #Start the bot
     updater.start_polling()
